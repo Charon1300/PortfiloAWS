fffdef264d560ab3211982307896e440de64d486
Charon1300 <150107890+Charon1300@users.noreply.github.com>
2026-06-27 01:00:44 -0400

    aws pages

---
+++ About.php
@@ -0,0 +1,28 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Charon's Portfilo</h1>
+    </header>
+
+    <nav id="TopNav">
+        <a href="PortfiloPage.php">Home</a>
+        <a href="Work.php">Works</a>
+        <a href="About.php">About</a>
+    </nav>
+
+    <section>
+        <h2>About me</h2>
+        <p>I'm Dillon Archer, I graduted from Rutgers Camden with a BA in computer science. I made this website to act as portfolio website</p>
+    </section>
+
+</body>
+</html>
---
+++ Work.php
@@ -0,0 +1,56 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Works</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Charon's Portfilo</h1>
+    </header>
+
+    <nav id="TopNav">
+        <a href="PortfiloPage.php">Home</a>
+        <a href="Work.php">Works</a>
+        <a href="About.php">About</a>
+    </nav>
+
+    <section>
+        <h1 id="Itch"><a class="Games" href="https://butcher-pete.itch.io/" target="_blank">my Itch.io page</a></h1>
+        <h2>Current released projects</h2>
+        <section id="GlizzyRoller">
+            <h3>Glizzy Roller</h3>
+            <img src="https://img.itch.zone/aW1nLzI1NTIxOTQ0LnBuZw==/315x250%23c/b07GGl.png" alt="Roller stake with ketchup and mustard on it" width="200" height="200">
+            <a class="Games" href="https://butcher-pete.itch.io/glizzy-roller" target="_blank">Play Now</a>
+            <p>Short roller staking platformer.</p>
+        </section>
+
+
+        <section id="Morpheus">
+            <h3>Morpheus' Helpers</h3>
+            <img src="https://img.itch.zone/aW1nLzE2MDQwNTQwLnBuZw==/315x250%23c/SRkryL.png" alt="Bird pixel art" width="200" height="200">
+            <a class="Games" href="https://butcher-pete.itch.io/morpheus-helpers" target="_blank">Play Now</a>
+            <p>A short puzzle game where you have to control a bird to deactive traps and guide a dog towards a flag. Made for a college class.</p>
+        </section>
+
+        <section id="Cross">
+            <h3>Crossflare</h3>
+            <img src="https://img.itch.zone/aW1nLzE4NTEzMjU5LnBuZw==/315x250%23c/nJmDvQ.png" alt="Pixel art of a dialognal cross" width="200" height="200">
+            <a class="Games" href="https://butcher-pete.itch.io/crossflare" target="_blank">Play Now</a>
+            <p>A simple short game about shooting targets with flares.</p>
+        </section>
+
+        <section id="When">
+            <h3>When, not if</h3>
+            <img src="https://img.itch.zone/aW1nLzI1MjMyMDYwLnBuZw==/315x250%23c/rPczsf.png" alt="damaged factory with with blue smoking coming out of the pipes" width="200" height="200">
+            <a class="Games" href="https://butcher-pete.itch.io/when-not-if" target="_blank">Play Now</a>
+            <p>A narrative mystery game</p>
+        </section>
+
+    </section>
+</body>
+</html>
