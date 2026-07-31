ed4bb33218d8746b7a2506c129e83f53606bcd75
Charon1300 <150107890+Charon1300@users.noreply.github.com>
2026-07-31 01:12:10 -0400

    icon nav top

--- index.php
+++ index.php
@@ -11,7 +11,42 @@
 
 <body>
     <header>
-        <h1>Charon's Portfilo</h1>
+       <div class = "flexContainer">
+            <div class = "flextitle">
+                <h1>Charon's Portfilo</h1>
+            </div>
+           
+            
+            <div class = "flexlink">
+                <a href = "https://bsky.app/profile/charondev.bsky.social" target = "_blank">
+                    <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/1280px-Bluesky_Logo.svg.png?utm_source=www.wikidata.org&utm_campaign=index&utm_content=thumbnail" width = "40" height = "40">
+
+                </a>
+            </div>
+
+            <div class = "flexlink">
+                <a href = "https://drive.google.com/file/d/1Szh0mUa_a04ZAWytef-Knphox5dfLGAZ/view?usp=sharing" target = "_blank">
+                    <!-- used https://www.flaticon.com/ for following icons  -->
+                    <img src = "https://cdn-icons-png.flaticon.com/128/4726/4726038.png" width = "40" height = "40">
+
+                </a>
+            </div>
+
+            <div class = "flexlink">
+                <a href = "https://www.linkedin.com/in/dillon-archer-514b75300/" target = "_blank">
+                    <img src = "https://cdn-icons-png.flaticon.com/128/3536/3536505.png" width = "40" height = "40">
+
+                </a>
+            </div>
+
+            <div class = "flexlink">
+                <a href = "mailto:dillonarcher111@gmail.com" target = "_blank">
+                    <img src = "https://cdn-icons-png.flaticon.com/128/18154/18154191.png" width = "40" height = "40">
+
+                </a>
+            </div>
+            
+        </div>
     </header>
 
     <nav id="TopNav">
